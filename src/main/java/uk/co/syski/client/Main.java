92368ba6559a69420f0028f60e77b3710554e765
JLScott1999 <git@jlscott.co.uk>
2019-02-12 14:41:45 +0000

    Initial Project Files

---
+++ src/main/java/uk/co/syski/client/Main.java
@@ -0,0 +1,16 @@
+package uk.co.syski.client;
+
+/**
+ * Main
+ * 
+ * @author JLScott1999
+ */
+public class Main
+{
+    
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
