11fdf8fadb17512e360797449751709617d03d90
Stedos <29103029+Stedoss@users.noreply.github.com>
2019-02-14 01:09:45 +0000

    Add Windows Interfaces to grab system data.

---
+++ src/main/java/uk/co/syski/client/Collection/CMDInterface.java
@@ -0,0 +1,28 @@
+package uk.co.syski.client.Collection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class CMDInterface
+{
+    public static void execute(String args) throws IOException
+    {
+        Runtime.getRuntime().exec(args);
+    }
+
+    public static String[] executeWithResponse(String args) throws IOException
+    {
+        String line;
+        Process process = Runtime.getRuntime().exec(args);
+        BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        LinkedList<String> output = new LinkedList<>();
+        while ((line = input.readLine()) != null)
+        {
+            output.add(line);
+        }
+        input.close();
+        return output.toArray(new String[output.size()]);
+    }
+}
---
+++ src/main/java/uk/co/syski/client/Collection/WMICInterface.java
@@ -0,0 +1,25 @@
+package uk.co.syski.client.Collection;
+
+import java.io.IOException;
+import uk.co.syski.client.Collection.CMDInterface;
+
+public class WMICInterface
+{
+    public static String[] getInfo(String component, String[] args) throws IOException
+    {
+        String commandArgs = "";
+        for (String s : args)
+            commandArgs += s + ",";
+        commandArgs = commandArgs.substring(0, commandArgs.length() - 1);
+
+        String[] outputValues = CMDInterface.executeWithResponse("wmic " + component + " get " + commandArgs + "/format:list");
+        String[] output = new String[args.length];
+
+        for (String line : outputValues)
+            for (int i = 0; i < args.length; i++)
+                if (line.toLowerCase().startsWith(args[i]))
+                    output[i] = line.substring(args[i].length() + 1);
+
+        return output;
+    }
+}
