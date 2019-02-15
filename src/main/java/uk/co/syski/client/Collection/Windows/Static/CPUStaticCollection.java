7938201e01b4366fc0071ca4d52a15af2b91fb93
Stedos <29103029+Stedoss@users.noreply.github.com>
2019-02-15 11:17:26 +0000

    Start adding CPU static collection.

--- src/main/java/uk/co/syski/client/Collection/Windows/Static/CPUStaticCollection.java
+++ src/main/java/uk/co/syski/client/Collection/Windows/Static/CPUStaticCollection.java
@@ -1,6 +1,20 @@
 package uk.co.syski.client.Collection.Windows.Static;
 
+import uk.co.syski.client.Collection.Windows.WMICInterface;
+import uk.co.syski.client.System.Components.Static.CPUStatic;
+
+import java.io.IOException;
+
 public class CPUStaticCollection
 {
+    public static CPUStatic getCPUStaticInfo()
+    {
+        CPUStatic cpu = new CPUStatic();
+        return cpu;
+    }
 
+    public static String getCPUName() throws IOException
+    {
+        return WMICInterface.getInfo("cpu", new String[] {"loadpercentage", "name"})[1];
+    }
 }
