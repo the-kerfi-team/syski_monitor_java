d5448c1fa2717c7084416a1da5fbd08a7215541a
Stedos <29103029+Stedoss@users.noreply.github.com>
2019-03-01 01:06:25 +0000

    Main and interface change.

--- src/main/java/uk/co/syski/client/Collection/Windows/WMICInterface.java
+++ src/main/java/uk/co/syski/client/Collection/Windows/WMICInterface.java
@@ -21,4 +21,35 @@ public class WMICInterface
 
         return output;
     }
+
+    public static String[][] getArrayInfo(String component, String[] args) throws IOException
+    {
+        String commandArgs = "";
+        for (String s : args)
+            commandArgs += s + ",";
+        commandArgs = commandArgs.substring(0, commandArgs.length() - 1);
+
+        String[] outputValues = CMDInterface.executeWithResponse("wmic " + component + " get " + commandArgs + "/format:list");
+        String[][] output = new String[args.length][arraySize(outputValues, args[0])];
+
+        for (int j = 0; j < arraySize(outputValues, args[0]); j++)
+            for (int c = 0; c < outputValues.length;  c++)
+                for (int i = 0, d = 0; i < args.length && d <= 0; i++)
+                    if (outputValues[c].toLowerCase().startsWith(args[i]))
+                    {
+                        output[j][i] = outputValues[c].substring(args[i].length() + 1);
+                        outputValues[c] = "";
+                        d++;
+                    }
+        return output;
+    }
+
+    private static int arraySize(String[] result, String key)
+    {
+        int count = 0;
+        for (String line : result)
+            if (line.toLowerCase().startsWith(key))
+                count++;
+        return count;
+    }
 }
--- src/main/java/uk/co/syski/client/Main.java
+++ src/main/java/uk/co/syski/client/Main.java
@@ -1,9 +1,25 @@
 package uk.co.syski.client;
 
+import uk.co.syski.client.Collection.Windows.WMICInterface;
+
+import java.io.IOException;
+
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        
+        /*try {
+            ConfigurationLoader.loadINI();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        ConfigurationLoader.loadAllConfigurations();
+        WebSocketClient wsc = new WebSocketClient(APIConfiguration.getURL() + ":" + APIConfiguration.getPort() + "/websocket", true);
+        wsc.start();*/
+
+        String[][] response = WMICInterface.getArrayInfo("memorychip", new String[] {"capacity"});
+        for (String[] s : response)
+            for (String str : s)
+                System.out.println(str);
     }
 }
