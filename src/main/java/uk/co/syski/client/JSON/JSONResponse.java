ae16e3ee9c50c4888bf0f57f6f5dc84f63491f90
Stedos <29103029+Stedoss@users.noreply.github.com>
2019-03-04 01:30:36 +0000

    Update JSON for some components.

--- src/main/java/uk/co/syski/client/JSON/JSONResponse.java
+++ src/main/java/uk/co/syski/client/JSON/JSONResponse.java
@@ -1,11 +1,10 @@
 package uk.co.syski.client.JSON;
 
 import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonArray;
 import com.eclipsesource.json.JsonObject;
 import uk.co.syski.client.System.CompleteSystemStatic;
-import uk.co.syski.client.System.Components.Static.CPUStatic;
-import uk.co.syski.client.System.Components.Static.OSStatic;
-import uk.co.syski.client.System.Components.Static.SystemStatic;
+import uk.co.syski.client.System.Components.Static.*;
 
 public class JSONResponse
 {
@@ -40,6 +39,24 @@ public class JSONResponse
                 .add("properties", getStaticOSJSON(os));
     }
 
+    public static JsonObject getJSON(RAMStatic[] ram)
+    {
+        return Json.object().add("action", "staticram")
+                .add("properties", Json.object().add("ram", getStaticRAMJSON(ram)));
+    }
+
+    public static JsonObject getJSON(MotherboardStatic mb)
+    {
+        return Json.object().add("action", "staticmotherboard")
+                .add("properties", getStaticMotherboardJSON(mb));
+    }
+
+    public static JsonObject getJSON(GPUStatic gpu)
+    {
+        return Json.object().add("action", "staticgpu")
+                .add("properties", getStaticGPUJSON(gpu));
+    }
+
     private static JsonObject getStaticCPUJSON(CPUStatic cpu)
     {
         return Json.object()
@@ -68,5 +85,35 @@ public class JSONResponse
                 .add("name", os.getName())
                 .add("version", os.getVersion())
                 .add("architecture", os.getArchitecture());
+    }
+
+    private static JsonArray getStaticRAMJSON(RAMStatic[] ram)
+    {
+        JsonArray array = new JsonArray();
+        for (RAMStatic r : ram)
+            array.add(
+                    Json.object()
+                    .add("model", r.getModel())
+                    .add("manufacturer", r.getManufacturer())
+                    .add("type", r.getType())
+                    .add("speed", r.getSpeed())
+                    .add("size", r.getSize())
+            );
+        return array;
+    }
+
+    private static JsonObject getStaticMotherboardJSON(MotherboardStatic mb)
+    {
+        return Json.object()
+                .add("model", mb.getModel())
+                .add("manufacturer", mb.getManufacturer())
+                .add("version", mb.getVersion());
+    }
+
+    private static JsonObject getStaticGPUJSON(GPUStatic gpu)
+    {
+        return Json.object()
+                .add("model", gpu.getModel())
+                .add("manufacturer", gpu.getManufacturer());
     }
 }
