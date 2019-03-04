a77e8f41b29f8bb9de6fa7a0a41e3a07f13dd094
Stedos <29103029+Stedoss@users.noreply.github.com>
2019-03-04 01:30:26 +0000

    Add collections for new static classes.

--- src/main/java/uk/co/syski/client/Collection/Windows/Static/Component/GPUStaticCollection.java
+++ src/main/java/uk/co/syski/client/Collection/Windows/Static/Component/GPUStaticCollection.java
@@ -1,6 +1,18 @@
 package uk.co.syski.client.Collection.Windows.Static.Component;
 
+import uk.co.syski.client.Collection.Windows.WMICInterface;
+import uk.co.syski.client.System.Components.Static.GPUStatic;
+
+import java.io.IOException;
+
 public class GPUStaticCollection
 {
-
+    public static GPUStatic GPUStaticInfo() throws IOException
+    {
+        GPUStatic gpu = new GPUStatic();
+        String[] result = WMICInterface.getInfo("PATH Win32_VideoController", new String[] {"name", "adaptercompatibility"});
+        gpu.setModel(result[0]);
+        gpu.setManufacturer(result[1]);
+        return gpu;
+    }
 }
---
+++ src/main/java/uk/co/syski/client/Collection/Windows/Static/Component/StorageStaticCollection.java
@@ -0,0 +1,28 @@
+package uk.co.syski.client.Collection.Windows.Static.Component;
+
+import uk.co.syski.client.Collection.Windows.WMICInterface;
+import uk.co.syski.client.System.Components.Static.StorageStatic;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class StorageStaticCollection
+{
+    public static StorageStatic[] getStorageStaticInfo() throws IOException
+    {
+        LinkedList<StorageStatic> storage = new LinkedList<>();
+        String[][] result = WMICInterface.getArrayInfo("diskdrive", new String[] {"caption", "manufacturer", "size", "inferfacetype"});
+        for (String[] str : result)
+        {
+            StorageStatic s = new StorageStatic();
+            s.setModel(str[0]);
+            s.setManufacturer(str[1]);
+            s.setSize(Long.parseLong(str[2]));
+            s.setType(str[3]);
+            storage.add(s);
+        }
+
+        StorageStatic[] arr = storage.toArray(new StorageStatic[storage.size()]);
+        return arr;
+    }
+}
