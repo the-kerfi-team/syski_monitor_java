86c2a0fd977bbcac28db4751262d8788f9417a5c
Stedos <29103029+Stedoss@users.noreply.github.com>
2019-03-10 18:23:00 +0000

    Implement threading into CPU variable.

--- src/main/java/uk/co/syski/client/Collection/Windows/Variable/Component/CPUVariableCollection.java
+++ src/main/java/uk/co/syski/client/Collection/Windows/Variable/Component/CPUVariableCollection.java
@@ -5,19 +5,56 @@ import uk.co.syski.client.System.Components.Variable.CPUVariable;
 
 import java.io.IOException;
 
-public class CPUVariableCollection
+public class CPUVariableCollection implements Runnable
 {
-    public static CPUVariable getCPUVariableInfo() throws IOException
+    private static final CPUVariable cpu = new CPUVariable();
+    private static Thread thread;
+    private static int sleepTime;
+    private static boolean threadActive = false;
+
+    public static CPUVariable getCurrentCPUVariableInfo()
     {
-        CPUVariable cpu = new CPUVariable();
-        cpu.setLoad(getLoadFromCommand());
-        cpu.setProcesses(CMDInterface.executeWithResponse("tasklist").length - 3);
         return cpu;
     }
 
-    private static float getLoadFromCommand() throws IOException
+    private static float getLoad() throws IOException
     {
         String[] result = CMDInterface.executeWithResponse("typeperf \"Processor(_Total)\\% Processor Time\" -sc 1");
         return Float.parseFloat(result[2].split("\",\"")[1].replace("\"", ""));
+    }
+
+    public static void startThreads(int sleep)
+    {
+        if (threadActive || thread != null)
+            throw new IllegalThreadStateException("Thread cannot be started while other thread of the same type is running.");
+        sleepTime = sleep;
+        threadActive = true;
+        Runnable runnable = new CPUVariableCollection();
+        thread = new Thread(runnable);
+        thread.start();
+    }
+
+    public static void stopThreads()
+    {
+        threadActive = false;
+    }
+
+    @Override
+    public void run()
+    {
+        while (threadActive)
+        {
+            try
+            {
+                cpu.setProcesses(CMDInterface.executeWithResponse("tasklist").length - 3);
+                cpu.setLoad(getLoad());
+                //Thread.sleep(sleepTime);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        thread = null;
     }
 }
