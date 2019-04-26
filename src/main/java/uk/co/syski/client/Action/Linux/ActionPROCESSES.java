53ed842ba3a3527c5c6ffd6d9dcde18627038a85
Stedos <29103029+Stedoss@users.noreply.github.com>
2019-04-26 02:35:19 +0100

    Add processes action for linux.

---
+++ src/main/java/uk/co/syski/client/Action/Linux/ActionPROCESSES.java
@@ -0,0 +1,12 @@
+package uk.co.syski.client.Action.Linux;
+
+import com.eclipsesource.json.JsonObject;
+import com.neovisionaries.ws.client.WebSocket;
+
+public class ActionPROCESSES extends uk.co.syski.client.Action.Generic.ActionPROCESSES
+{
+    public ActionPROCESSES(WebSocket websocket, JsonObject properties)
+    {
+        super(websocket, properties);
+    }
+}
