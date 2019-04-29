a07e3991a2adbc250dc8da60db89cec208d81afc
Stedos <29103029+Stedoss@users.noreply.github.com>
2019-04-29 14:19:55 +0100

    Add linux kill process action.

---
+++ src/main/java/uk/co/syski/client/Action/Linux/ActionKILLPROCESS.java
@@ -0,0 +1,35 @@
+package uk.co.syski.client.Action.Linux;
+
+import com.eclipsesource.json.JsonObject;
+import com.neovisionaries.ws.client.WebSocket;
+import uk.co.syski.client.Action.Action;
+import uk.co.syski.client.Collection.CMDInterface;
+import uk.co.syski.client.JSON.JSONResponse;
+import uk.co.syski.client.System.Components.Static.BIOSStatic;
+import uk.co.syski.client.util.Output;
+
+public class ActionKILLPROCESS extends Action
+{
+    public ActionKILLPROCESS(WebSocket websocket, JsonObject properties)
+    {
+        super(websocket, properties);
+    }
+
+    @Override
+    public void execute()
+    {
+        Output.printLineToConsole("[ACTION] - Kill Process");
+        if (properties != null)
+        {
+            try
+            {
+                if (properties.get("id") != null || !properties.get("id").equals("0"))
+                    CMDInterface.execute("kill -9 " + properties.get("id"));
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
